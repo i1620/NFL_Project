d321abaf325edca1ff8461d9c5dad6ebd631a20e
i1620 <68018020+i1620@users.noreply.github.com>
2021-01-09 12:35:38 -0600

    made changes to app.py

--- app.py
+++ app.py
@@ -16,32 +16,3 @@ port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
     app.run(debug=True, port=port)
     app.run(host="0.0.0.0")  # allows access from any computer
-=======
-from flask import Flask,render_template,request
- 
-app = Flask(__name__)
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-
-@app.route('/teamPlays.html')
-def teamPlays():
-    return render_template('teamPlays.html')
-
-
-@app.route('/form')
-def form():
-    return render_template('form.html')
- 
-@app.route('/data', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form = request.form
-     
-        return render_template('data.html',form = form)
- 
- 
-app.run(host='localhost', port=5000)
->>>>>>> d5c86d240252237bd1dd6f494225845695200809
