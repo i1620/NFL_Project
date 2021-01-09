1d196456bd44d715ff903bdd629d60508c81151b
i1620 <68018020+i1620@users.noreply.github.com>
2021-01-09 12:49:08 -0600

    made some minor changes

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 import os
 
@@ -8,8 +7,6 @@ app = Flask(__name__)
 @app.route('/<html>')
 def home(html="index.html"):
     return render_template(html)
-
-
 
 port = int(os.environ.get('PORT', 5000))
 
